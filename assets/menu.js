d0af77342072093a8b1408030196c14d1d5b60bb
extenv <im.ivan.maulana@gmail.com>
2026-01-26 22:50:34 +0700

    Genexus Docs Update

---
+++ assets/menu.js
@@ -0,0 +1,18 @@
+window.menuData = [
+  {
+    title: "Getting Started",
+    icon: "play",
+    submenus: [
+      { name: "Installation Guide", file: "installation.md" },
+      {
+        name: "System Requirements",
+        file: "/docs/getting-started/requirements.md",
+      },
+    ],
+  },
+  {
+    title: "Development",
+    icon: "code-2",
+    submenus: [{ name: "Generators", file: "generators.md" }],
+  },
+];
